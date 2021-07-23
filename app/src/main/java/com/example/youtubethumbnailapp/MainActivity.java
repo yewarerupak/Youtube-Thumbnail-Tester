9b0cfddf66e4af68d36118a10693b0f5e1243864
rupakyeware <rupakyeware@gmail.com>
2021-07-23 13:01:21 +0530

    Home page design

--- app/src/main/java/com/example/youtubethumbnailapp/MainActivity.java
+++ app/src/main/java/com/example/youtubethumbnailapp/MainActivity.java
@@ -1,16 +1,59 @@
 package com.example.youtubethumbnailapp;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-import com.example.youtubethumbnailapp.R;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView video1;
+
+    public Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initialize variables
+        video1 = findViewById(R.id.video1);
+
+        video1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                choosePicture();
+            }
+        });
+    }//end of onCreate
+
+    private void choosePicture() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==1 && resultCode==RESULT_OK && null != data && data.getData()!=null){
+            imageUri = data.getData();
+            video1.setImageURI(imageUri);
+            addPicture();
+        }
+    }
+
+    private void addPicture(){
+        Picasso.get()
+                .load(imageUri)
+                .fit().centerCrop()
+                .into(video1);
     }
 }
